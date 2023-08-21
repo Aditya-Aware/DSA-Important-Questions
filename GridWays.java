135d58726ba6645057386fddbfb08b348836ae1b
Aditya Aware <101728767+Aditya-Aware@users.noreply.github.com>
2023-08-22 01:32:59 +0530

    Add files via upload

---
+++ GridWays.java
@@ -0,0 +1,20 @@
+package ImpQuestions;
+
+public class GridWays {
+    public static int girdWays(int i, int j, int n, int m){
+        //base
+        if (i==n-1 && j==m-1){
+            return 1;
+        } else if (i==n||j==n){
+            return 0;
+        }
+        int w1 = girdWays(i+1, j, n,m);
+        int w2= girdWays(i, j+1, n, m);
+        return w1+w2;
+    }
+
+    public static void main(String[] args) {
+        int n=3, m=3;
+        System.out.println(girdWays(0,0,n,m));
+    }
+}
